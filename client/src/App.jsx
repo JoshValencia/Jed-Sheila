c9e43ffbf3840649164aedafb93ecca34f2e28cf
JoshValencia <jushuavalencia@gmail.com>
2024-01-05 12:17:50 +0800

    update client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  PlasmicRootProvider,
+  PlasmicComponent,
+} from "@plasmicapp/loader-react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { PLASMIC } from "./plasmic-init";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <PlasmicRootProvider loader={PLASMIC}>
+      <Router>
+        <Routes>
+          <Route path="/" element={CatchAllPage()} />
+        </Routes>
+      </Router>
+    </PlasmicRootProvider>
+  );
 }
 
-export default App
+export function CatchAllPage() {
+  const [loading, setLoading] = useState(true);
+  const [pageData, setPageData] = useState(null);
+
+  useEffect(() => {
+    async function load() {
+      const pageData = await PLASMIC.maybeFetchComponentData(location.pathname);
+      setPageData(pageData);
+      setLoading(false);
+    }
+    load();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!pageData) {
+    return <div>Not found</div>;
+  }
+  // The page will already be cached from the `load` call above.
+  return <PlasmicComponent component={location.pathname} />;
+}
+
+export default App;
